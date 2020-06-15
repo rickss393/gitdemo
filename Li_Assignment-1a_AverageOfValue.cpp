e9d707a280e01485771242ceccec41c0a0398303
rickless8842 <bbg8842@live.com>
2020-06-15 16:47:23 -0700

    upload Li_Assi_1a

---
+++ Li_Assignment-1a_AverageOfValue.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a=28,b=32,c=37,d=24,e=33,sum;
+    sum=a+b+c+d+e;
+    cout<<"The sum is "<<sum<<endl;
+    cout<<"The average is "<<sum/5<<endl;
+
+    return 0;
+}
